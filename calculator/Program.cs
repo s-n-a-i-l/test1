6f21a62d80c72da9a0a2f619edef236f2672aa23
sevru <sevru@DESKTOP-NFMFIIS>
2025-05-12 20:41:42 +1000

    qreeew

---
+++ calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выражение(+-*/):");
+          
+            double number = Convert.ToDouble(Console.ReadLine());
+            char op = Convert.ToChar(Console.ReadLine());
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+
+            switch (op)
+            {
+                case '+':
+                    result = number + number2;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case '-':
+                    result = number - number2;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case '*': 
+                    result = number * number2;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case '/':
+                    if (number == 0 || number2 == 0)
+                        Console.WriteLine("Ошибка операция / на нуль или нуль на число невозможна.");
+
+                    result = number / number2;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+
+            }
+
+            // double number = Convert.ToDouble(Console.ReadLine());
+          
+        }
+    }
+}
---
+++ keyWASD/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace keyWASD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int x = 10; // начальная позиция по X
+                int y = 10; // начальная позиция по Y
+                ConsoleKeyInfo key;
+
+                Console.CursorVisible = false;
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(x, y);
+                    Console.Write("<3"); // символ курсора
+
+                    key = Console.ReadKey(true);
+
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.W:
+                        case ConsoleKey.UpArrow:
+                            if (y > 0) y--;
+                            break;
+                        case ConsoleKey.S:
+                        case ConsoleKey.DownArrow:
+                            if (y < Console.WindowHeight - 1) y++;
+                            break;
+                        case ConsoleKey.A:
+                        case ConsoleKey.LeftArrow:
+                            if (x > 0) x--;
+                            break;
+                        case ConsoleKey.D:
+                        case ConsoleKey.RightArrow:
+                            if (x < Console.WindowWidth - 1) x++;
+                            break;
+                        case ConsoleKey.Escape:
+                            return; // выход из программы
+                    }
+
+                    Thread.Sleep(50); // задержка, чтобы не было слишком быстро
+                }
+            }
+        }
+    }
+}
