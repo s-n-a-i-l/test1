7390432b43446bfe460e7bb5ad247241a560e75c
sevru <sevru@DESKTOP-NFMFIIS>
2025-05-14 20:34:14 +1000

    dz3 DONE chess geometry2

--- Geomertry2/Program.cs
+++ Geomertry2/Program.cs
@@ -10,27 +10,54 @@ namespace Geomertry2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-            int n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите размер ромба: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < n; i++)
-            { 
-              for (int j = i; j< n; j++)  Console.Write(" ");  Console.Write("/");
+            //for (int i = 0; i < n; i++)
+            //{ 
+            //  for (int j = i; j< n; j++)  Console.Write(" ");  Console.Write("/");
                 
-              for(int j = 0; j < i*2; j++)  Console.Write(" "); Console.Write("\\"); 
+            //  for(int j = 0; j < i*2; j++)  Console.Write(" "); Console.Write("\\"); 
 
-                Console.WriteLine();
-            }
+            //    //Console.WriteLine();
 
-            for (int i = 0;i < n; i++) 
+            //    for (int j = 0; j <= i; j++) Console.Write(" "); Console.Write("\\");
+
+            //    for (int j = 0; j < (n - i - 1) * 2; j++) Console.Write(" "); Console.Write("/");
+
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0;i < n; i++) 
+            //{
+
+            //}
+
+            Console.Write("Введите размер ромба: ");
+            int n = int.Parse(Console.ReadLine());//парсим в инт, чтоб нельзя было вводить буковы,,,,очень удобно сразу с проверкой
+
+            for (int i = 0; i < 2 * n - 1; i++)
             {
-              for(int j = 0; j<= i; j++) Console.Write(" "); Console.Write("\\");
 
-              for(int j = 0;j < (n-i-1)*2; j++) Console.Write(" "); Console.Write("/");
+                if (i < n) // верх ромба
+                {
+                    
+                    for (int j = 0; j < n - i - 1; j++) Console.Write(" "); Console.Write("/");
+                    
+                    for (int j = 0; j < i * 2; j++) Console.Write(" "); Console.Write("\\");
+                }
+                else // низ ромба
+                {
+                    int line = i - n;
+       
+                    for (int j = 0; j < line; j++) Console.Write(" "); Console.Write("\\");
+                    
+                    for (int j = 0; j < 2 * (n - line - 1); j++) Console.Write(" "); Console.Write("/");
+                }
 
                 Console.WriteLine();
             }
-            
+
         }
     }
 }
---
+++ chess/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер шахматной доски: ");
+            int boardSize = int.Parse(Console.ReadLine());
+
+            //for (int rowBlock = 0; rowBlock < boardSize; rowBlock++)
+            //{
+            //    for (int line = 0; line < size; line++)
+            //    {
+            //        for (int colBlock = 0; colBlock < boardSize; colBlock++)
+            //        {
+            //            bool isBlack = (rowBlock + colBlock) % 2 == 0;
+            //            for (int ch = 0; ch < size; ch++)
+            //            {
+            //                Console.Write(isBlack ? "*" : " ");
+            //            }
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
+
+            for (int i = 0; i < boardSize; i++) 
+            {
+              for (int j = 0; j < boardSize; j++) 
+                {
+
+                    if ((i + j) % 2 == 0)//если четное серное если нет пробел
+                        Console.Write("■ ");
+                    else
+                        Console.Write("  ");
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
