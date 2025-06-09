e71738d6464228d0946aed5c0d76f5d837bd5e43
sevru <sevru@DESKTOP-NFMFIIS>
2025-06-10 00:16:01 +1000

    culcul done

--- culcul/Program.cs
+++ culcul/Program.cs
@@ -6,100 +6,141 @@ using System.Threading.Tasks;
 
 namespace culcul
 {
-    internal class Program
-    {
+	class Program
+	{
+		static char[] operations = new char[] { '+', '-', '*', '/', '(', ')' };
+		static char[] digits = "0123456789. ".ToCharArray();
+		static void Main(string[] args)
+		{
+			//Console.Write("Введите арифметическое выражение: ");
+			string expression = "(22+33)-44/2+6*8"; //Console.ReadLine();            
+			string[] s_values = expression.Split(Program.operations);
+			double[] values = new double[s_values.Length];
+			for (int i = 0; i < values.Length; i++)
+			{
+				Console.Write(values[i] + "\t");
+				values[i] = Convert.ToDouble(s_values[i]);
+			}
+			Console.WriteLine();
+			string[] operations = expression.Split(Program.digits);
+			operations = operations.Where(o => o != "").ToArray();
+			for (int i = 0; i < operations.Length; i++)
+			{
+				Console.Write(operations[i] + "\t");
+			}
+			Console.WriteLine();
 
-        static char[] op = new char[] { '+', '-', '/', '*' };//массив возможных операций
-        static char[] digits = "0123456789. ".ToCharArray();//--\\-- но числа
-        static void Main(string[] args)
-        {
-
-            //Console.Write("Введите арифметическое выражение: ");
-            string expression = "22+33-44/2+6*8";
-            //Console.Write(expression);
-            string[] s_values = expression.Split(op);
-            double[] values = new double[s_values.Length];
-            for (int i = 0; i < s_values.Length; i++)
+			#region Simple
+			/*double a = Convert.ToDouble(values[0]);
+			double b = Convert.ToDouble(values[1]);
+			char current_operation = expression[expression.IndexOfAny(Program.operations)];
+            Console.Write(expression);
+            switch (current_operation)
             {
-                Console.Write($"{s_values[i] + '\t'}");
-                values[i] = Convert.ToDouble(s_values[i]);
-            }
-            Console.WriteLine();
+                case '+': Console.WriteLine($" = {a + b}"); break;
+                case '-': Console.WriteLine($" = {a - b}"); break;
+                case '*': Console.WriteLine($" = {a * b}"); break;
+                case '/': Console.WriteLine($" = {a / b}"); break;
+                default: Console.WriteLine("Error: no operation!"); break;
+			}*/
+			#endregion
 
-            string[] ops = expression.Split(digits);
-            ops = ops.Where(o => o != "").ToArray();
+			while (operations.Contains("("))
+			{
+				for (int i = 0; i < operations.Length; i++)
+				{
+					if (operations[i] == "(" && operations[i] == ")" && operations[i] == "+")
+					{
+						if (operations[0] == "+") values[0] += values[1];
+						if (operations[0] == "+")
+						{
+							for (int j = 1; j < operations.Length; j++)
+							{
+								operations[j - 1] = operations[j];
+								values[j] = values[j + 1];
+							}
+							if (operations[operations.Length - 1] != " ")
+							{
+								operations[operations.Length - 1] = " ";
+								values[values.Length - 1] = 0;
+							}
+						}
+					}
+				}
+			}
+			for (int i = 0; i < values.Length; i++)
+			{
+				Console.Write(values[i] + "\t");
+			}
+			Console.WriteLine();
+			for (int i = 0; i < operations.Length; i++)
+			{
+				Console.Write(operations[i] + "\t");
+			}
+			Console.WriteLine();
 
-            for (int i = 0; i < ops.Length; i++)
-            {
-                Console.Write($"{ops[i] + "\t"}");
-            }
-            Console.WriteLine();
+			while (operations.Contains("*") || operations.Contains("/"))
+			{
+				for (int i = 0; i < operations.Length; i++)
+				{
+					if (operations[i] == "*") values[i] *= values[i + 1];
+					if (operations[i] == "/") values[i] /= values[i + 1];
+					if (operations[i] == "*" || operations[i] == "/")
+					{
+						for (int j = i + 1; j < operations.Length; j++)
+						{
+							operations[j - 1] = operations[j];
+							values[j] = values[j + 1];
+						}
+						if (operations[operations.Length - 1] != " ")
+						{
+							operations[operations.Length - 1] = " ";
+							values[values.Length - 1] = 0;
+						}
+					}
+				}
+			}
+			for (int i = 0; i < values.Length; i++)
+			{
+				Console.Write(values[i] + "\t");
+			}
+			Console.WriteLine();
+			for (int i = 0; i < operations.Length; i++)
+			{
+				Console.Write(operations[i] + "\t");
+			}
+			Console.WriteLine();
 
-
-            #region Simple
-            /* double a = Convert.ToDouble(values[0]);
-                double b = Convert.ToDouble(values[1]);// возможно массив со значениями и вытаскивать по индексу
-
-                Console.Write(expression);
-                char current_op = expression[expression.IndexOfAny(op)];
-                switch (current_op) 
-                {
-                 case '+': Console.WriteLine($" = {a + b}");break;
-                 case '-': Console.WriteLine($" = {a - b}"); break;
-                 case '/': Console.WriteLine($" = {a / b}"); break;
-                 case '*': Console.WriteLine($" = {a * b}"); break;
-                 default: Console.WriteLine("error: no operation");break;
-                }*/
-            #endregion
-
-            while (ops.Contains("*") || ops.Contains("/"))
-            {
-                for (int i = 0; i < ops.Length; i++)
-                {
-                    if (ops[i] == "*") values[i] *= values[i + 1];
-                    if (ops[i] == "/") values[i] /= values[i + 1];
-
-                    if (ops[i] == "*" || ops[i] == "/")
-                    {
-                        for (int j = i + 1; j < ops.Length; j++)
-                        {
-                            ops[j - 1] = ops[j];
-                            values[j] = values[j + 1];
-                        }
-                        if (ops[ops.Length - 1] != " ")
-                        {
-                            ops[ops.Length - 1] = " ";
-                            values[values.Length - 1] = 0;
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i < values.Length; i++) Console.Write(values[i] + "\t"); Console.WriteLine();
-            for (int i = 0; i < ops.Length; i++) Console.Write(ops[i] + "\t"); Console.WriteLine(); 
-            
-            Console.WriteLine();
-            while (ops.Contains("+") || ops.Contains("-"))
-            {
-                
-                    if (ops[0] == "+") values[0] += values[1];
-                    if (ops[0] == "-") values[0] -= values[1];
-                    if (ops[0] == "+" || ops[0] == "-")
-                    {
-                        for (int j = 1; j < ops.Length; j++)
-                        {
-                            ops[j - 1] = ops[j];
-                            values[j] = values[j + 1];
-                        }
-                        if (ops[ops.Length - 1] != " ")
-                        {
-                            ops[ops.Length - 1] = " ";
-                            values[values.Length - 1] = 0;
-                        }
-                    }
-                
-            }
-            for (int i = 0; i < values.Length; i++) Console.Write(values[i] + "\t"); Console.WriteLine();
-            for (int i = 0; i < ops.Length; i++) Console.Write(ops[i] + "\t"); Console.WriteLine();
-        }
-    }
+			while (operations.Contains("+") || operations.Contains("-"))
+			{
+				for (int i = 0; i < operations.Length; i++)
+				{
+					if (operations[0] == "+") values[0] += values[1];
+					if (operations[0] == "-") values[0] -= values[1];
+					if (operations[0] == "+" || operations[0] == "-")
+					{
+						for (int j = 1; j < operations.Length; j++)
+						{
+							operations[j - 1] = operations[j];
+							values[j] = values[j + 1];
+						}
+						if (operations[operations.Length - 1] != " ")
+						{
+							operations[operations.Length - 1] = " ";
+							values[values.Length - 1] = 0;
+						}
+					}
+				}
+			}
+			for (int i = 0; i < values.Length; i++)
+			{
+				Console.Write(values[i] + "\t");
+			}
+			Console.WriteLine();
+			for (int i = 0; i < operations.Length; i++)
+			{
+				Console.Write(operations[i] + "\t");
+			}
+		}
+	}
 }
